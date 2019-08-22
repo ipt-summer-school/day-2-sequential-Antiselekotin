d5815b6b26f34c2261fac01a1ff3b54d8ad25c64
Antiselekotin <deksazon8@gmail.com>
2019-08-23 00:26:56 +0300

    ununderstoodable shit

---
+++ HW.js
@@ -0,0 +1,14 @@
+let numbers = [34, 51, 12, 41];
+
+
+// function firstWaterFall (arr) {
+//     let l = []
+//     for (let i = 0; arr.lenght; i++){
+//         l[i] = Math.SQRT(arr[i])
+//     }
+//     return(l)
+// }
+// console.log(firstWaterFall(numbers))
+
+console.log(numbers.forEach(numbers++))
+
