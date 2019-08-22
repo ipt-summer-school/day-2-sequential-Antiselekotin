1072306144175a70c637edbbea17d61e1b6ea2ce
Antiselekotin <deksazon8@gmail.com>
2019-08-23 01:27:31 +0300

    ohhh yeah, i've done it

--- HW.js
+++ HW.js
@@ -1,14 +1,33 @@
-let numbers = [34, 51, 12, 41];
+let numbers = [34, 51, 12, 41],
+    secNumbers = [],
+    trdNumbers = [];
 
+function firstWaterFall (arr) {
+    let chArr = []
+    for (let i = 0; i < arr.length; i++){
+    chArr.push(arr[i] + 6)
+}
+    return (chArr)
+}
+secNumbers = (firstWaterFall(numbers)) //передаем значения функции firstWaterFall в переменную secNumbers для дальнейшего преобразования
+function secondWaterFall (arr) {
+    let chArr = [];
+    for(let i = 0; i < arr.length; i++){
+    chArr.push(arr[i]*4)
+    }
+    return(chArr)
+}
+trdNumbers = secondWaterFall(secNumbers); //аналогично с прошлым коментариемб, передаем переменную
+function thirdWaterFall (arr) {
+    let chArr = [];
+    for(let i = 0; i < arr.length; i++){
+        chArr.push(Math.round(arr[i]/3))
+    }
+    return(chArr)
+}
+finalResult = thirdWaterFall(trdNumbers);
 
-// function firstWaterFall (arr) {
-//     let l = []
-//     for (let i = 0; arr.lenght; i++){
-//         l[i] = Math.SQRT(arr[i])
-//     }
-//     return(l)
-// }
-// console.log(firstWaterFall(numbers))
-
-console.log(numbers.forEach(numbers++))
-
+console.log(numbers)
+console.log(secNumbers)
+console.log(trdNumbers)
+console.log(finalResult)
